4232209be10c7007a74902bb3e93294f1c2acac1
Nikhil Nigade <nikhil@dezinezync.com>
2019-11-11 14:18:06 +0530

    various minor fixes
    - fixed showing double dates (when post was edited)
    - fixed padding for the post-footer
    - uses a different background color for the main footer

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -33,9 +33,9 @@ if ( ! function_exists( 'elytra_time_link' ) ) :
  */
 function elytra_time_link() {
 	$time_string = '<time class="entry-date published updated" datetime="%1$s">%2$s</time>';
-	if ( get_the_time( 'U' ) !== get_the_modified_time( 'U' ) ) {
-		$time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time><time class="updated" datetime="%3$s">%4$s</time>';
-	}
+	// if ( get_the_time( 'U' ) !== get_the_modified_time( 'U' ) ) {
+	// 	$time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time><time class="updated" datetime="%3$s">%4$s</time>';
+	// }
 
 	$time_string = sprintf( $time_string,
 		get_the_date( DATE_W3C ),
